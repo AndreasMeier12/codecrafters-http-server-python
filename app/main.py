b489b0ff231036eaffa092e82b5f1cb1b01ffbca
Andreas Meier <meier89@gmx.ch>
2025-04-06 14:49:43 +0200

    Add compression headers

--- app/main.py
+++ app/main.py
@@ -4,11 +4,18 @@ import threading
 from dataclasses import dataclass
 import re
 import argparse
+from typing import Optional
 
-HTTP___NOT_FOUND_ = b"HTTP/1.1 404 Not Found\r\n\r\n"
+HTTP___NOT_FOUND_ = "HTTP/1.1 404 Not Found"
 
 ENCODING = 'utf-8'
 
+
+@dataclass
+class HttpResponse:
+    status_line: str
+    headers: dict[str, str]
+    body: Optional[bytes]
 
 @dataclass
 class ServerArguments:
@@ -24,31 +31,50 @@ class HttpRequest:
     user_agent: str
     accept: str
     body: str
+    accept_encoding: Optional[str]
 
 
-def handle_file(request_data: HttpRequest, server_args: ServerArguments) -> bytes:
+def handle_file(request_data: HttpRequest, server_args: ServerArguments) -> HttpResponse:
     file_name = request_data.path.replace("/files/", server_args.file_dir)
 
     if request_data.method == 'GET':
         if not os.path.isfile(file_name):
-            return HTTP___NOT_FOUND_
+            return HttpResponse(HTTP___NOT_FOUND_, {}, None)
         size = os.path.getsize(file_name)
         with open(file_name, 'rb') as file:
-            return f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {size}\r\n\r\n".encode(ENCODING) + file.read()
+            return HttpResponse("HTTP/1.1 200 OK", {"Content-Type": "application/octet-stream", "Content-Length": size}, file.read())
     if request_data.method == 'POST':
         with open(file_name, 'wb') as file:
             file.write(request_data.body.encode(ENCODING))
-        return "HTTP/1.1 201 Created\r\n\r\n".encode(ENCODING)
+        return HttpResponse("HTTP/1.1 201 Created", {}, None)
 
 
 
 
-def handle_echo(request_data: HttpRequest) -> bytes:
+def handle_echo(request_data: HttpRequest) -> HttpResponse:
     arg = re.sub("^/echo/", "", request_data.path)
-    return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(arg)}\r\n\r\n{arg}".encode(ENCODING)
+    headers = {
+        "Content-Type": "text/plain",
+        "Content-Length": len(arg)
+    }
+    return HttpResponse("HTTP/1.1 200 OK", headers, arg.encode(ENCODING))
 
 def handle_user_agent(request_dat: HttpRequest):
-    return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(request_dat.user_agent)}\r\n\r\n{request_dat.user_agent}".encode(ENCODING)
+    return HttpResponse("HTTP/1.1 200 OK", {"Content-Type": "text/plain", "Content-Length": len(request_dat.user_agent)}, request_dat.user_agent.encode(ENCODING))
+
+def format_http_response(response: HttpResponse) -> bytes:
+    res = response.status_line.encode(ENCODING) + "\r\n".encode(ENCODING)
+    for key, val in response.headers.items():
+        res = res + f"{key}: {val}\r\n".encode(ENCODING)
+    res = res + "\r\n".encode(ENCODING)
+    if response.body:
+        res = res + response.body
+    return res
+
+
+def handle_compression(request: HttpRequest, response: HttpResponse):
+    if request.accept_encoding == 'gzip':
+        response.headers['Content-Encoding'] = 'gzip'
 
 
 def parse_http_request(data: bytes):
@@ -65,11 +91,11 @@ def parse_http_request(data: bytes):
             more_parts_dict[components[0]] = components[1]
         else:
             body = components[0]
-    return HttpRequest(method, path, protocol, more_parts_dict.get('HOST', None), more_parts_dict.get('User-Agent', None), more_parts_dict.get('Accept', None), body)
+    return HttpRequest(method, path, protocol, more_parts_dict.get('HOST', None), more_parts_dict.get('User-Agent', None), more_parts_dict.get('Accept', None), body, more_parts_dict.get("Accept-Encoding", None))
 
-def handle_request_content(request_data: HttpRequest, server_args: ServerArguments ) -> bytes:
+def handle_request_content(request_data: HttpRequest, server_args: ServerArguments ) -> HttpResponse:
     if request_data.path == '/':
-        return b"HTTP/1.1 200 OK\r\n\r\n"
+        return HttpResponse("HTTP/1.1 200 OK", {}, None)
     if request_data.path.startswith("/echo/"):
         return handle_echo(request_data)
     if request_data.path.startswith('/files/'):
@@ -78,12 +104,15 @@ def handle_request_content(request_data: HttpRequest, server_args: ServerArgumen
         return handle_user_agent(request_data)
 
 
-    return HTTP___NOT_FOUND_
+    return HttpResponse(HTTP___NOT_FOUND_, {}, None)
 
 def handle_request(conn: socket, server_args: ServerArguments):
     data = conn.recv(1024)
     request_data = parse_http_request(data)
-    conn.sendall(handle_request_content(request_data, server_args))
+    temp = handle_request_content(request_data, server_args)
+    handle_compression(request_data, temp)
+
+    conn.sendall(format_http_response(temp))
 
 
 def main():
